829e428e28f8c1c3f920e01948d0bb1fef0a36f2
fum1n0 <g1521025@outlook.com>
2017-10-28 14:51:29 +0900

    プロジェクト ファイルを追加します。

---
+++ RealTimeEqualizer/cpp/Main.cpp
@@ -0,0 +1,19 @@
+﻿
+#include"../hpp/FilterEditorFreq.hpp"
+
+
+void Main(){
+
+	const Font font(20);
+	ScalableWindow::Setup(600, 480);
+	Window::SetTitle(L"Filter Editer");
+
+	std::shared_ptr<FilterEditorFreq>fef = std::make_shared<FilterEditorFreq>();
+
+	while (System::Update()) {
+
+		fef->update();
+
+
+	}
+}
